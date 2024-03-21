22ab2434c1026ab895ff48f1bfe10d7e010e9c15
Wangzhe Sun <wangzhe.sun@outlook.com>
2024-03-21 03:24:35 -0500

    modify underlying model of preemption probability prediction

--- Models/preemption_estimator.py
+++ Models/preemption_estimator.py
@@ -9,7 +9,16 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
+# import numpy as np
+# import pandas as pd
+# from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score, classification_report
+
 import threading
+
+SLEEP_TIME = 5
 
 class PreemptionEstimator(object):
     def __init__(self, max_price=-1):
@@ -22,8 +31,14 @@ class PreemptionEstimator(object):
         self.start_time = datetime.datetime.now()
         # self.time_deltas = np.empty()
         # self.prices = np.empty()
-        self.time_deltas = np.array([1,2,3,4]).reshape(-1, 1)
-        self.prices = np.array([1,2,3,4]).reshape(-1, 1)
+        self.time_deltas = np.array([])
+        self.prices = np.array([])
+        self.exceeds = np.array([])
+    
+    def add_data(self):
+        self.time_deltas = np.append(self.time_deltas, [1,2,3,4])
+        self.prices = np.append(self.prices, [1,2,3,4])
+        self.exceeds = np.append(self.exceeds, [0,0,1,0])
     
     def get_spot_instance_price(self, region, instance_type, curr_time):
         client = boto3.client('ec2', region_name=region)
@@ -47,45 +62,47 @@ class PreemptionEstimator(object):
             if price != None:
                 self.time_deltas = np.append(self.time_deltas, curr_time.timestamp())
                 self.prices = np.append(self.prices, price)
+                if float(price) > self.max_price:
+                    self.exceeds = np.append(self.exceeds, 1)
+                else:
+                    self.exceeds = np.append(self.exceeds, 0)
             
             print(self.time_deltas)
             print(self.prices)
+            print(self.exceeds)
 
             if self.predict_intention:
                 self.mutex_lock.release()
-                time.sleep(30)
+                time.sleep(SLEEP_TIME)
 
                 self.mutex_lock.acquire()
             else:
-                time.sleep(30)
+                time.sleep(SLEEP_TIME)
     
     def compute_preempt_prob_without_max(self):
         # TODO: how to estimate preemption probability if preemption does not rely on price
         return 0.005
 
     def compute_preempt_prob_with_max(self):
-        degree = 2 
-
-        # get current time_delta
         current_time = datetime.datetime.now()
-        x_val = (current_time - self.start_time).total_seconds()
 
-        # define regression model
-        poly = PolynomialFeatures(degree)
-        x_train_poly = poly.fit_transform(self.time_deltas)
-        x_predict_poly = poly.transform(np.array([current_time.timestamp()]).reshape(-1, 1))
+        # Standardize features
+        scaler = StandardScaler()
+        X_train_scaled = scaler.fit_transform(self.time_deltas.reshape(-1,1))
+        y_train = self.exceeds
 
-        model = LinearRegression()
-        model.fit(x_train_poly, self.prices)
+        # print(self.time_deltas.reshape(-1,1))
+        print(X_train_scaled)
+        print(y_train)
+        # Train logistic regression model
+        model = LogisticRegression()
+        model.fit(X_train_scaled, y_train)
 
-        price_predict = model.predict(x_predict_poly)
-
-        # print(current_time.timestamp())
-        # print(price_predict)
-
-        # TODO: estimate probability from the price
-        # Challenge 1: hard to customize for each instance type and location
-        # challenge 2: how to continually fit in new data without retrain
+        # Predict probability for a specific time
+        specific_time = (current_time-self.start_time).total_seconds()
+        specific_time_feature = scaler.transform([[specific_time]])
+        probability = model.predict_proba(specific_time_feature)[0][1]
+        print(f"Probability of price exceeding the function value at time {specific_time}: {probability}")
     
     def compute_preemption_prob(self):
         self.predict_intention = True
@@ -103,7 +120,9 @@ class PreemptionEstimator(object):
 
 
 P = PreemptionEstimator(10)
-P.poll_price()
-# P.compute_preemption_prob()
+# P.poll_price()
+
+P.add_data()
+P.compute_preemption_prob()
 
 # print(P.prob)
